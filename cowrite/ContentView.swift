11afcfd1d96a6e801c3236689b3e71cc303bcef5
paulflanagan <PaulFlanagan@protonmail.com>
2024-06-10 22:26:30 -0400

    Initial Commit

---
+++ cowrite/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cowrite
+//
+//  Created by Paul Flanagan on 6/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cowrite/cowriteApp.swift
@@ -0,0 +1,17 @@
+//
+//  cowriteApp.swift
+//  cowrite
+//
+//  Created by Paul Flanagan on 6/10/24.
+//
+
+import SwiftUI
+
+@main
+struct cowriteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
